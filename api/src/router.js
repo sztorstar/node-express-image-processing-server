0c45bec1e92489f1a2a7064ec8419a53d092325c
Shu Zhang <szhang@torstar.ca>
2022-08-03 16:53:32 -0400

    finish module 3

---
+++ api/src/router.js
@@ -0,0 +1,32 @@
+const { Router } = require('express')
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: 'api/uploads/',
+    filename: filename
+})
+const router = Router()
+
+function filename(request, file, callback){
+    callback(null, file.originalname)
+}
+
+function fileFilter(request, file, callback){
+    if(file.mimetype !== 'image/png'){
+        request.fileValidationError = 'Wrong file type'
+        callback(null, false, new Error('Wrong file type'))
+    }else{
+        callback(null, true)
+    }
+}
+
+const upload = multer({fileFilter, storage})
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if(request.fileValidationError){
+        return response.status(400).json({error: request.fileValidationError})
+    }else{
+        return response.status(201).json({success: true})
+    }
+})
+
+module.exports = router;
