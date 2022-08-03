2f345cd4bf2e98463fac354714b3d8ad6a1f5d19
Shu Zhang <szhang@torstar.ca>
2022-08-03 15:50:33 -0400

    fix export

--- api/app.js
+++ api/app.js
@@ -5,6 +5,4 @@ const pathToIndex = path.resolve(__dirname, '../client/index.html');
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex)
 })
-module.exports = {
-    app
-}
+module.exports = app
