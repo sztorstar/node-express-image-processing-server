afac0624c98f82d66cfefff4390b44f202a06ccb
Shu Zhang <szhang@torstar.ca>
2022-08-03 16:55:07 -0400

    change the order

--- api/app.js
+++ api/app.js
@@ -4,9 +4,8 @@ const router = require('./src/router');
 const app = express()
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
 app.use('/', router)
-
+app.use(express.static(path.resolve(__dirname, 'uploads')))
 app.use('/*', (request, response) => {
     response.sendFile(pathToIndex)
 })
-app.use(express.static(path.resolve(__dirname, 'uploads')))
 module.exports = app
